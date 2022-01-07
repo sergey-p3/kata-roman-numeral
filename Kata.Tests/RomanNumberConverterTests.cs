8980ad2725c60842924e596ca711a9e556c748cc
Sergey Pusnei <sergey.pusnei@gmail.com>
2022-01-07 11:14:35 -0500

    Added UT for 3

--- Kata.Tests/RomanNumberConverterTests.cs
+++ Kata.Tests/RomanNumberConverterTests.cs
@@ -23,6 +23,7 @@ namespace Kata.Tests
         [Theory]
         [InlineData(1, "I")]
         [InlineData(2, "II")]
+        [InlineData(3, "III")]
         public void GivenA1_ShouldReturnAnI(int input, string expected)
         {
            var roman =  _converter.Convert(input);
